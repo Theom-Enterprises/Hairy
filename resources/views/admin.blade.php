c96888fd1d455a896e547b3e87edb6c9c5166ac4
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2021-12-02 12:16:16 +0100

    [1.2.2.2.5] Adminoberfläche entwickeln [1.2.2.1.7] Adminpage entwickeln Hinzufügen einer Übersicht der Angestellten

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,7 +10,7 @@
                 <th scope="col">Dienstleistung</th>
                 <th scope="col">Friseur</th>
                 <th scope="col">Kunde</th>
-                <th scope="col">Löschen</th>
+                <th scope="col">Bearbeiten</th>
             </tr>
             </thead>
             <tbody>
@@ -22,10 +22,12 @@
                 ->get()->toArray();
 
             /*
+            TODO Erstellen der SQL Tables
             $termine = DB::table('termin')
                 ->join('users', 'users.id', '=', 'termin.user_id')
                 ->join('dienstleistung', 'dienstleistung.id', '=', 'termin.dienstleistung_id')
                 ->join('angestellter', 'angestellter.id', '=', 'termin.angestellter_friseurkuerzel')
+                ->orderBy('datum')
                 ->select('datum', 'dienstleistung.bezeichnung', 'users.vorname', 'users.nachname', 'users.firstname', 'users.lastname')
                 ->get()->toArray();
             */
@@ -39,6 +41,35 @@
                 echo "<td><i class=\"bi bi-x-square-fill\"></i></td>";
                 echo "</tr>";
             }
+            ?>
+            </tbody>
+        </table>
+
+        <h1>Friseure</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Kürzel</th>
+                <th scope="col">Name</th>
+                <th scope="col">Anstellungsdatum</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+                /*
+                 TODO Erstellen des SQL Tables Angestellter
+            $angestellte = DB::table('angestellter')
+                ->select('friseurkuerzel', 'vorname', 'nachname', 'erstelldatum')
+                ->get()->toArray();
+
+            foreach ($angestellte as $angestellter) {
+                echo "<tr>";
+                echo "<td>{{ $angestellter->friseurkuerzel }}</td>";
+                echo "<td>{{ \"{$angestellter->vorname} {$angestellter->nachname}\" }}</td>";
+                echo "<td>{{ $angestellter->erstelldatum }}</td>";
+                echo "</tr>";
+            }
+                */
             ?>
             </tbody>
         </table>
