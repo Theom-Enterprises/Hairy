12d041562735ed49b2f60734a996335b9c4b3e69
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2021-12-06 13:10:57 +0100

    1.2.2.1.7 Adminpage entwickeln Anpassen von php Code an Laravel Hinzufügen des Reiters "Von - Bis"

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -18,38 +18,40 @@
                     <tr>
                         <th scope="col"><i class="bi bi-pencil-fill"></i></th>
                         <th scope="col">Datum</th>
+                        <th scope="col">Von - Bis</th>
                         <th scope="col">Dienstleistung</th>
                         <th scope="col">Friseur</th>
                         <th scope="col">Kunde</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php
-                    $users = DB::table('users')
-                        ->select('firstname', 'lastname')
-                        ->get()->toArray();
+                    @php
+                        $users = DB::table('users')
+                            ->select('firstname', 'lastname')
+                            ->get()->toArray();
 
-                    /*
-                    TODO Erstellen der SQL Tables
-                    $termine = DB::table('termin')
-                        ->join('users', 'users.id', '=', 'termin.user_id')
-                        ->join('dienstleistung', 'dienstleistung.id', '=', 'termin.dienstleistung_id')
-                        ->join('angestellter', 'angestellter.id', '=', 'termin.angestellter_friseurkuerzel')
-                        ->orderBy('datum')
-                        ->select('datum', 'dienstleistung.bezeichnung', 'users.vorname', 'users.nachname', 'users.firstname', 'users.lastname')
-                        ->get()->toArray();
-                    */
+                        /*
+                        TODO Erstellen der SQL Tables
+                        $termine = DB::table('termin')
+                            ->join('users', 'users.id', '=', 'termin.user_id')
+                            ->join('dienstleistung', 'dienstleistung.id', '=', 'termin.dienstleistung_id')
+                            ->join('angestellter', 'angestellter.id', '=', 'termin.angestellter_friseurkuerzel')
+                            ->orderBy('datum')
+                            ->select('datum', 'dienstleistung.bezeichnung', 'users.vorname', 'users.nachname', 'users.firstname', 'users.lastname')
+                            ->get()->toArray();
+                        */
+                    @endphp
 
-                    foreach ($users as $user) {
-                        echo "<tr>";
-                        echo "<td><i class=\"bi bi-x-circle-fill\" style='color: #DC3545'></i></td>";
-                        echo "<td>02.12.2021</td>";
-                        echo "<td>Haare färben</td>";
-                        echo "<td>Max Mustermann</td>";
-                        echo "<td>{{ \"{$user->firstname} {$user->lastname}\" }}</td>";
-                        echo "</tr>";
-                    }
-                    ?>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td><i class="bi bi-x-circle-fill" style='color: #DC3545'></i></td>
+                            <td>02.12.2021</td>
+                            <td>10:15 - 10:45</td>
+                            <td>Haare schneiden</td>
+                            <td>Max Mustermann</td>
+                            <td>{{ "{$user->firstname} {$user->lastname}" }}</td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
 
@@ -63,22 +65,21 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <?php
-                    /*
-                     TODO Erstellen des SQL Tables Angestellter
-                $angestellte = DB::table('angestellter')
-                    ->select('friseurkuerzel', 'vorname', 'nachname', 'erstelldatum')
-                    ->get()->toArray();
+                    {{-- TODO Erstellen des SQL Tables Angestellter
+                    @php
+                    $angestellte = DB::table('angestellter')
+                        ->select('friseurkuerzel', 'vorname', 'nachname', 'erstelldatum')
+                        ->get()->toArray();
+                    @endphp
 
-                foreach ($angestellte as $angestellter) {
-                    echo "<tr>";
-                    echo "<td>{{ $angestellter->friseurkuerzel }}</td>";
-                    echo "<td>{{ \"{$angestellter->vorname} {$angestellter->nachname}\" }}</td>";
-                    echo "<td>{{ $angestellter->erstelldatum }}</td>";
-                    echo "</tr>";
-                }
-                    */
-                    ?>
+                    @foreach ($angestellte as $angestellter)
+                        <tr>
+                            <td>{{ $angestellter->friseurkuerzel }}</td>
+                            <td>{{ "{$angestellter->vorname} {$angestellter->nachname}" }}</td>
+                            <td>{{ $angestellter->erstelldatum }}</td>
+                        </tr>
+                    @endforeach
+                    --}}
                     </tbody>
                 </table>
             </div>
