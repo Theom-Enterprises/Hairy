f3c048487cc01aff7f226d7754933a2a19f818cb
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2021-12-15 16:44:51 +0100

    1.2.2.1.7 Adminpage entwickeln Hinzufügen einer "Visitenkarte"

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -23,7 +23,7 @@
                         </p>
                     </div>
                     <div class="row">
-                        <p class="h1 col">Alexander Sarka</p>
+                        <p class="h1 col">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</p>
                     </div>
                     <div class="row">
                         <p class="h4 col">Rennweg 89b, 1030 Wien</p>
@@ -49,31 +49,40 @@
                                 ->select('firstname', 'lastname')
                                 ->get()->toArray();
 
-                            /*
-                            TODO Erstellen der SQL Tables
+/*
                             $termine = DB::table('termin')
                                 ->join('users', 'users.id', '=', 'termin.user_id')
                                 ->join('dienstleistung', 'dienstleistung.id', '=', 'termin.dienstleistung_id')
                                 ->join('angestellter', 'angestellter.id', '=', 'termin.angestellter_friseurkuerzel')
                                 ->orderBy('datum')
-                                ->select('datum', 'dienstleistung.bezeichnung', 'users.vorname', 'users.nachname', 'users.firstname', 'users.lastname')
+                                ->select('datum', 'von', 'bis','dienstleistung.bezeichnung', 'users.vorname', 'users.nachname', 'angestellter.firstname', 'angestellter.lastname')
                                 ->get()->toArray();
-                            */
+*/
                         @endphp
 
-                        @foreach ($users as $user)
+                        @foreach ($termine as $termin)
                             <tr>
+
+                                {{--                                <td><i class="bi bi-x-circle-fill" style='color: #DC3545'></i></td>--}}
+                                {{--                                <td>{{ $termine->datum }}</td>--}}
+                                {{--                                <td>{{ "{$termine->von} - {$termine->bis}" }}</td>--}}
+                                {{--                                <td>{{ $termine->dienstleitung->bezeichnung }}</td>--}}
+                                {{--                                <td>{{ "{$termine->angestellter->vorname} {$termine->angestellter->lastname}" }}</td>--}}
+                                {{--                                <td>{{ "{$termine->users->vorname} {$termine->users->vorname}" }}</td>--}}
+
                                 <td><i class="bi bi-x-circle-fill" style='color: #DC3545'></i></td>
-                                <td>02.12.2021</td>
-                                <td>10:15 - 10:45</td>
+                                <td>{{ $termin->datum }}</td>
+                                <td>10:15 - 10:32</td>
                                 <td>Haare schneiden</td>
-                                <td>Max Mustermann</td>
-                                <td>{{ "{$user->firstname} {$user->lastname}" }}</td>
+                                <td>Omar Faid</td>
+                                <td>Kunde Name</td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
+                </div>
 
+                <div class="tables">
                     <h2>Friseure</h2>
                     <table class="table table-responsive table-hover table-borderless">
                         <thead>
@@ -84,6 +93,16 @@
                         </tr>
                         </thead>
                         <tbody>
+                        <tr>
+                            <td>SAR</td>
+                            <td>Alexander Sarka</td>
+                            <td>10.01.2000</td>
+                        </tr>
+                        <tr>
+                            <td>LOZ</td>
+                            <td>Alejandro Lozada</td>
+                            <td>17.02.2000</td>
+                        </tr>
                         {{-- TODO Erstellen des SQL Tables Angestellter
                         @php
                         $angestellte = DB::table('angestellter')
--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin', function()
-{
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+
+Route::get('/admin', function () {
     return view('admin');
 });
 
