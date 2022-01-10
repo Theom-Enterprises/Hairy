b099bc8928f4325303c269c7f8ba0aefcab9325d
Daniel Lozada <daniellozada1702@gmail.com>
2022-01-10 21:10:42 +0100

    1.2.2.1.6 Adminpage entwickeln - Updaten der Routen

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::domain(env('APP_URL'))
-    ->group(function () {
-        Route::get('/', function () {
-            return view('index');
-        });
-    });
+Route::get('/', function () {
+    return view('index');
+});
+Route::redirect('/home', '/');
 
 Auth::routes();
 
