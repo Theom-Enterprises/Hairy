d2740539da07863e27e422bb12ad27a1b1493a4e
Daniel Lozada <daniellozada1702@gmail.com>
2022-01-10 02:31:51 +0100

    1.2.2.1.6 Adminpage entwickeln - Hinzufügen von logout controller für das Abmelden von einem Angestellten

---
+++ app/Http/Controllers/Auth/AdminLogoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLogoutController extends Controller
+{
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('employee')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect(route('admin.login'));
+    }
+}
