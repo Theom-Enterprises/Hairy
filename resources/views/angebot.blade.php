1b988355187739734b4e816358d2bf15727ec435
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2022-01-11 09:27:02 +0100

    1.2.2.2.7 Dienstleistungspage entwickeln CSS überarbeiten Suchleiste hinzufügen

--- resources/views/angebot.blade.php
+++ resources/views/angebot.blade.php
@@ -4,8 +4,8 @@
     <link href="{{ asset('css/angebot.css') }}" rel="stylesheet">
 @endpush
 
-@push('body-js')
-
+@push('scripts')
+    <script defer src="/js/angebot.js"></script>
 @endpush
 
 @section('subtitle', 'Angebot')
@@ -17,25 +17,20 @@
             Unsere Angebote
         </h1>
 
-        <div class="p-2 my-4" style="border: solid #dee2e6">
+        <div id="title-div">
             <div class="row">
-                <div class="col m-2 d-flex justify-content-start align-items-center">
-                    <button type="button" class="btn btn-primary">
-                        <a href="#">Termin buchen</a>
-                    </button>
-                </div>
                 <div class="col m-2 d-flex justify-content-end align-items-center">
-                    <div class="input-group">
-                        <input type="text" class="form-control input-text" placeholder="Nach Angebot suchen"
+                    <div id="search-bar" class="input-group">
+                        <input id="search-bar-input" onkeyup="sucheAngebot()" type="text"
+                               class="form-control input-text"
+                               placeholder="Nach Angebot suchen..."
                                aria-label="Nach Angebot suchen" aria-describedby="searchButton">
-                        <button class="btn btn-outline-warning btn-lg" type="button" id="searchButton">
-                            <i class="bi bi-search"></i>
-                        </button>
+                        <i class="btn btn-outline-warning btn-lg bi bi-search"></i>
                     </div>
                 </div>
             </div>
             <div id="angebote" class="table-responsive table-angebot">
-                <table class="table table-hover table-borderless">
+                <table id="table-angebote" class="table table-hover table-borderless">
                     <thead>
                     <tr>
                         <th scope="col">Bezeichnung</th>
@@ -54,6 +49,11 @@
                     @endforeach
                     </tbody>
                 </table>
+            </div>
+            <div class="col m-2 d-flex justify-content-start align-items-center">
+                <button type="button" id="btn-buchung" class="btn btn-primary">
+                    <a href="#">Termin buchen</a>
+                </button>
             </div>
         </div>
 @endsection
