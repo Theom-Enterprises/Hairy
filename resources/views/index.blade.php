badce46e65c13d354ab2ed651ae37221a42a20bf
Omar Faid <8132@htl.rennweg.at>
2022-01-23 18:47:32 +0100

    index.php - footer hinzugefügt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -166,4 +166,10 @@
             </div>
         </div>
     </section>
+    <footer class="flex-shrink-0">
+        <div class="container-fluid text-muted text-center">
+            <span>Made in Austria with love</span>
+            <p class="mt-1 mb-3">&copy; Hairy 2021–2022</p>
+        </div>
+    </footer>
 @endsection
