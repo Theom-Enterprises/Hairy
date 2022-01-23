b4f1f0bd9e0ab6fc45632ca88a98e6b18d9d3fd2
Omar Faid <8132@htl.rennweg.at>
2022-01-23 17:28:53 +0100

    app.blade.php - sidebar-icon aktualisiert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,9 @@
         <div class="container-fluid mx-5">
             @auth
                 <button type="button" id="sidebarCollapse" class="btn">
-                    <i class="bi bi-layout-sidebar" style="font-size: 1.3rem;"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
                 </button>
             @endauth
 
