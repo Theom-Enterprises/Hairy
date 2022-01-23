b9b257ce2c05f942304da7409f31010728a4272e
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2022-01-23 21:46:06 +0100

    1.2.2.1.6 Adminpage entwickeln Automatisches Befüllen der ID bei Angestellten hinzufügen

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -308,14 +308,6 @@
                                                    name="erstelldatum"
                                                    required>
                                         </div>
-                                        <div class="mb-3">
-                                            <label for="friseursalon" class="form-label">Friseursalon
-                                                ID</label>
-                                            <input type="number" min="1" class="form-control"
-                                                   id="friseursalon"
-                                                   name="friseursalon_id"
-                                                   required>
-                                        </div>
                                         <div class="mb-3 form-check">
                                             <input type="checkbox" class="form-check-input" id="ist_admin"
                                                    name="ist_admin">
