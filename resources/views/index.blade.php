ea04cb93c9253ea6c993f058e8bcbef75b89c1a5
Alexander Sarka <alexander.sarka@htl.rennweg.at>
2022-01-23 16:55:47 +0100

    1.2.2.2.1 Landing Page entwickeln Fix SVG

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
                 </div>
             </div>
         </div>
-        <svg id="svg-header-ending" class="flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg class="flex-shrink-0 svg-margin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#f3f4f5" fill-opacity="1" d="M0,192L1440,128L1440,320L0,320Z"></path>
         </svg>
     </header>
@@ -112,7 +112,7 @@
                 </div>
             </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="svg-margin">
             <path fill="#f3f4f5" fill-opacity="1" d="M0,192L1440,128L1440,320L0,320Z"></path>
         </svg>
     </section>
